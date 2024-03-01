b12616cdf5815ecde57876cc9191cba75836e2a5
Vasilii Kletkin <kletkinvasilii@gmail.com>
2024-03-02 01:20:28 +0700

    add fix

--- backend/dialogs/apps.py
+++ backend/dialogs/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class DialogsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dialogs'
-    
+
     def ready(self):
         from . import tasks
