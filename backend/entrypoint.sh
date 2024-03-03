d3e0f6a344b8ffecfd2a2e1ca172cefc454e196c
Vasilii Kletkin <kletkinvasilii@gmail.com>
2024-03-03 22:18:03 +0700

    add fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-sleep 2
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# python manage.py flush --no-input
 python manage.py migrate
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 exec "$@"
