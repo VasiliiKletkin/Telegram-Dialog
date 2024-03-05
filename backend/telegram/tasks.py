feefbfa5ee6333e2f3a283f7e6b9e5c4e48104fa
Vasilii Kletkin <kletkinvasilii@gmail.com>
2024-03-05 20:27:11 +0700

    add fix

--- backend/telegram/tasks.py
+++ backend/telegram/tasks.py
@@ -27,6 +27,9 @@ def check_user(id):
                 proxy=telegram_user.proxy_server.get_proxy_dict(),
             )
 
+            if not telegram_client.is_user_authorized():
+                raise Exception
+
             async with telegram_client:
                 await telegram_client.get_me()
                 await telegram_client.get_dialogs()
