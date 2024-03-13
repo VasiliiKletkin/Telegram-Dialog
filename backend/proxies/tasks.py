5e638d2c98fb6718af1adbf7220c108a85fd2b64
Vasilii Kletkin <kletkinvasilii@gmail.com>
2024-03-13 20:16:52 +0700

    add fix

--- backend/proxies/tasks.py
+++ backend/proxies/tasks.py
@@ -4,16 +4,10 @@ from core.celery import app
 from .models import ProxyServer
 
 
-print()
-
-
 @app.task
 def check_proxy(id):
     proxy = ProxyServer.objects.get(id=id)
-
-    proxies = {
-        'http': str(proxy)
-    }
+    proxies = {'http': str(proxy)}
     response = requests.get('http://www.httpbin.org/ip', proxies=proxies,)
     resp_data = response.json()
     proxy.is_active = True if resp_data["origin"] == proxy.address else False
