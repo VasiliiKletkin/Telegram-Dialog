bff1d4c318081e4bafe82913724ecdd6f676b7df
Vasilii Kletkin <kletkinvasilii@gmail.com>
2024-03-19 15:48:28 +0700

    empty commit

--- backend/telegram/models.py
+++ backend/telegram/models.py
@@ -13,8 +13,7 @@ class TelegramGroup(TimeStampedModel):
 
 
 class TelegramGroupMessage(models.Model):
-    group = models.ForeignKey(
-        TelegramGroup, on_delete=models.CASCADE)
+    group = models.ForeignKey(TelegramGroup, on_delete=models.CASCADE)
     message_id = models.BigIntegerField()
     user_id = models.BigIntegerField()
     reply_to_msg_id = models.BigIntegerField()
@@ -30,20 +29,21 @@ class TelegramUser(TimeStampedModel):
 
     id = models.BigIntegerField(primary_key=True)
     username = models.CharField(max_length=32, null=True, blank=True)
-    first_name = models.CharField(
-        default="", max_length=64, null=True, blank=True)
-    last_name = models.CharField(
-        default="", max_length=64, null=True, blank=True)
+    first_name = models.CharField(default="", max_length=64, null=True, blank=True)
+    last_name = models.CharField(default="", max_length=64, null=True, blank=True)
     phone = models.CharField(max_length=30, null=True, blank=True)
     two_fa = models.CharField(max_length=30, null=True, blank=True)
 
-    app = models.OneToOneField(
-        App, on_delete=models.CASCADE)
-    client_session = models.OneToOneField(
-        ClientSession, on_delete=models.CASCADE)
+    app = models.OneToOneField(App, on_delete=models.CASCADE)
+    client_session = models.OneToOneField(ClientSession, on_delete=models.CASCADE)
 
     proxy_server = models.OneToOneField(
-        ProxyServer, on_delete=models.SET_NULL, null=True, blank=True, related_name="telegram_user")
+        ProxyServer,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="telegram_user",
+    )
 
     app_json = models.JSONField(null=True, blank=True)
     error = models.TextField(null=True, blank=True)
@@ -53,4 +53,8 @@ class TelegramUser(TimeStampedModel):
 
     @property
     def is_ready(self):
-        return (self.proxy_server.is_ready and self.is_active) if self.proxy_server else False
+        return (
+            (self.proxy_server.is_ready and self.is_active)
+            if self.proxy_server
+            else False
+        )
