c3d6c77072e9bf056c48ae1f3a2f43af416c1730
Vasilii Kletkin <kletkinvasilii@gmail.com>
2024-03-25 15:53:55 +0700

    unique

--- backend/telegram/models.py
+++ backend/telegram/models.py
@@ -10,9 +10,6 @@ class TelegramGroup(TimeStampedModel):
     name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True)
 
-    class Meta:
-        unique_together = "username"
-
     def __str__(self):
         return f"{self.name} - @{self.username}"
 
