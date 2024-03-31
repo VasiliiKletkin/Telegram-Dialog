122a54b152ee96d6917a0853cefdedb0a150ece3
Vasilii Kletkin <kletkinvasilii@gmail.com>
2024-03-31 13:06:12 +0700

    add fix

--- backend/dialogs/views.py
+++ backend/dialogs/views.py
@@ -7,14 +7,15 @@ from .models import Message, Dialog, Scene
 class MessageRoleNameAutocomplete(autocomplete.Select2ListView):
 
     def get_list(self):
-        qs = Message.objects.values_list("role_name", flat=True)
+        qs = Message.objects.values_list("id", "role_name").distinct()
 
         if dialog := self.forwarded.get("dialog", None):
             qs = qs.filter(dialog=dialog)
 
         if self.q:
             qs = qs.filter(Q(role__icontains=self.q))
-        return qs.distinct()
+        return qs
+
 
 class MessageAutocomplete(autocomplete.Select2QuerySetView):
     queryset = Message.objects.all()
@@ -22,11 +23,14 @@ class MessageAutocomplete(autocomplete.Select2QuerySetView):
 
 
 class DialogAutocomplete(autocomplete.Select2QuerySetView):
-    queryset = Dialog.objects.filter(is_active=True)
+    queryset = Dialog.objects.all()
     search_fields = ["name", "id"]
 
     def get_queryset(self):
         qs = super().get_queryset()
+        if is_active := self.forwarded.get("is_active"):
+            qs = qs.filter(is_active=is_active)
+
         if telegram_group := self.forwarded.get("telegram_group"):
             scenes = Scene.objects.filter(telegram_group=telegram_group)
             qs = qs.exclude(scenes__in=scenes)
