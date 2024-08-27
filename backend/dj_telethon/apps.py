a3bed26da3dff09d0c5cdb3568424736907629f3
Vasilii Kletkin <kletkinvasilii@gmail.com>
2024-08-27 20:30:10 +0300

    added django telethon

--- backend/dj_telethon/apps.py
+++ backend/dj_telethon/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class DjTelethonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dj_telethon'
+
+    verbose_name = _("Django Telethon")
+    verbose_name_plural = _("Django Telethon")
+
+    def ready(self):
+        from .receivers import receiver_telegram_client_registered  # noqa: F401
